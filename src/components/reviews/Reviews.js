73041d8965e181a70b9374e4e3ad8b81eda2a21d
Eyeruz <3yerus@gmail.com>
2020-10-11 23:14:04 -0400

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -5,7 +5,11 @@ class Reviews extends Component {
   render() {
     // let review;
 
-    const review = this.props.reviews.map(rev => <Review key={rev.id} review={rev} />)
+    const review = this.props.reviews.map(rev => <Reviews
+      reviews={this.props.reviews}
+      restaurantId={this.props.restaurant.id}
+      deleteReview={this.props.deleteReview}
+     />)
     // const rev = this.props.reviews.map(rev => rev.id)
     // if(rev === this.props.restaurantId){
     //   return review = this.props.reviews.map(rev => <Review key={rev.id} review={rev}/>)
@@ -20,3 +24,4 @@ class Reviews extends Component {
 };
 
 export default Reviews;
+
